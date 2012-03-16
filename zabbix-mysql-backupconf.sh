371beaf02b709ccb8c9436eda97d97ad75ce5412
maxhq <jens@jebecs.de>
2012-03-16 15:55:21 +0100

    CHG Backup of Zabbix 1.9.x / 2.0.0 CHG Incorporated mysqldump options suggested by Jonathan Bayer REM unnecessary use of variables (DATEBIN etc) for commands that use to be in $PATH

--- zabbix-mysql-backupconf.sh
+++ zabbix-mysql-backupconf.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 #
 # zabbix-mysql-backupconf.sh
+# v0.4 - 20120302 Incorporated mysqldump options suggested by Jonathan Bayer
+# v0.3 - 20120206 Backup of Zabbix 1.9.x / 2.0.0, removed unnecessary use of
+#                 variables (DATEBIN etc) for commands that use to be in $PATH
 # v0.2 - 20111105
 #
-# Configuration Backup for Zabbix 1.8 w/MySQL
+# Configuration Backup for Zabbix 2.0 w/MySQL
 #
 # Author: Ricardo Santos (rsantos at gmail.com)
 # http://zabbixzone.com
 #
+# modified by Jens Berthold, 2012
+#
 # Thanks for suggestions from:
 # - Oleksiy Zagorskyi (zalex)
 # - Petr Jendrejovsky
+# - Jonathan Bayer
 #
 
 # mysql config
@@ -19,53 +25,52 @@ DBNAME="zabbix"
 DBUSER="zabbix"
 DBPASS="YOURMYSQLPASSWORDHERE"
 
-# some tools
-MYSQLDUMP="`which mysqldump`"
-GZIP="`which gzip`"
-DATEBIN="`which date`"
-MKDIRBIN="`which mkdir`"
-
 # target path
 MAINDIR="/var/lib/zabbix/backupconf"
-DUMPDIR="${MAINDIR}/`${DATEBIN} +%Y%m%d%H%M`"
-${MKDIRBIN} -p ${DUMPDIR}
+DUMPDIR="${MAINDIR}/`date +%Y%m%d-%H%M`"
+
+mkdir -p "${DUMPDIR}"
 
 # configuration tables
-CONFTABLES=( actions applications autoreg_host conditions config dchecks dhosts \
-drules dservices escalations expressions functions globalmacro graph_theme \
-graphs graphs_items groups help_items hostmacro hosts hosts_groups \
-hosts_profiles hosts_profiles_ext hosts_templates housekeeper httpstep \
-httpstepitem httptest httptestitem ids images items items_applications \
-maintenances maintenances_groups maintenances_hosts maintenances_windows \
-mappings media media_type node_cksum nodes opconditions operations \
-opmediatypes profiles proxy_autoreg_host proxy_dhistory proxy_history regexps \
-rights screens screens_items scripts service_alarms services services_links \
-services_times sessions slides slideshows sysmaps sysmaps_elements \
-sysmaps_link_triggers sysmaps_links timeperiods trigger_depends triggers \
-user_history users users_groups usrgrp valuemaps )
+CONFTABLES=( actions applications autoreg_host conditions config dchecks \
+dhosts drules dservices escalations expressions functions globalmacro \
+globalvars graph_discovery graph_theme graphs graphs_items groups help_items \
+host_inventory hostmacro hosts hosts_groups hosts_templates housekeeper \
+httpstep httpstepitem httptest httptestitem icon_map icon_mapping ids images \
+interface item_discovery items items_applications maintenances \
+maintenances_groups maintenances_hosts maintenances_windows mappings media \
+media_type node_cksum nodes opcommand opcommand_grp opcommand_hst opconditions \
+operations opgroup opmessage opmessage_grp opmessage_usr optemplate profiles \
+proxy_autoreg_host proxy_dhistory proxy_history regexps rights screens \
+screens_items scripts service_alarms services services_links services_times \
+sessions slides slideshows sysmap_element_url sysmap_url sysmaps \
+sysmaps_elements sysmaps_link_triggers sysmaps_links timeperiods \
+trigger_depends trigger_discovery triggers user_history users users_groups \
+usrgrp valuemaps )
 
 # tables with large data
 DATATABLES=( acknowledges alerts auditlog_details auditlog events \
 history history_log history_str history_str_sync history_sync history_text \
 history_uint history_uint_sync trends trends_uint )
 
+DUMPFILE="${DUMPDIR}/zbx-conf-bkup-`date +%Y%m%d-%H%M`.sql"
+>"${DUMPFILE}"
+
 # CONFTABLES
 for table in ${CONFTABLES[*]}; do
-        DUMPFILE="${DUMPDIR}/${table}.sql"
-        echo "Backuping table ${table}"
-        ${MYSQLDUMP} -R --opt --extended-insert=FALSE \
-                -h ${DBHOST} -u ${DBUSER} -p${DBPASS} ${DBNAME} --tables ${table} >${DUMPFILE}
-        ${GZIP} -f ${DUMPFILE}
+	echo "Backuping table ${table}"
+	mysqldump -R --opt --single-transaction --skip-lock-tables --extended-insert=FALSE \
+		-h ${DBHOST} -u ${DBUSER} -p${DBPASS} ${DBNAME} --tables ${table} >>"${DUMPFILE}"
 done
 
 # DATATABLES
 for table in ${DATATABLES[*]}; do
-        DUMPFILE="${DUMPDIR}/${table}.sql"
-        echo "Backuping schema table ${table}"
-        ${MYSQLDUMP} -R --opt --no-data	\
-                -h ${DBHOST} -u ${DBUSER} -p${DBPASS} ${DBNAME} --tables ${table} >${DUMPFILE}
-        ${GZIP} -f ${DUMPFILE}
+	echo "Backuping schema table ${table}"
+	mysqldump -R --opt --single-transaction --skip-lock-tables --no-data	\
+		-h ${DBHOST} -u ${DBUSER} -p${DBPASS} ${DBNAME} --tables ${table} >>"${DUMPFILE}"
 done
+
+gzip -f "${DUMPFILE}"
 
 echo
 echo "Backup Completed - ${DUMPDIR}"
